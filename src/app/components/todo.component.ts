b4cd2a55d63159d4b741036c99f154315676d8b0
Lu Khei <kheiboardwarrior@gmail.com>
2020-11-26 15:54:58 +0800

    clean up

--- src/app/components/todo.component.ts
+++ src/app/components/todo.component.ts
@@ -52,19 +52,17 @@ export class TodoComponent implements OnInit {
   }
 
   delete(i: number) {
- 
+
     this.tasksArray.removeAt(i)
 
   }
 
   get todo(): Todo {
-
     return this.toDoForm.value as Todo
   }
 
 
   @Input() set todo(t: Todo) {
-    //leave it
     if (t) {
       t.tasks.forEach(task => {
         const eachTask = this.fb.group({
@@ -84,18 +82,4 @@ export class TodoComponent implements OnInit {
 
   }
 
-  ELEMENT_DATA = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  ];
-  dataSource = this.ELEMENT_DATA
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 }
